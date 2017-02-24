ee12212482c251be4d8d5f77dc8bd82e063d127b
Nuro <smithycleave@gmail.com>
2017-02-24 21:56:47 +0000

    Re-added install.php
    Numpty :(
    
    Signed-off-by: Nuro <smithycleave@gmail.com>

--- install.php
+++ install.php
@@ -1,2 +1,6 @@
 <?php
-
+include('config.php');
+//ALTER TABLE
+//ALTER TABLE sightings ADD last_updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+$altertable = $db->pdo->prepare('ALTER TABLE sightings ADD last_updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP');
+$altertable->execute();
