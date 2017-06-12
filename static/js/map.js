57761b4871849783e38c984a95700d0689b0ee18
Glenn Carremans <glenn.carremans@appwise.be>
2017-06-12 15:14:44 +0200

    Added 10 seconds timeout to ajax request

--- static/js/map.js
+++ static/js/map.js
@@ -953,6 +953,7 @@ function loadRawData() {
     return $.ajax({
         url: 'raw_data',
         type: 'GET',
+        timeout: 10000,
         data: {
             'timestamp': timestamp,
             'pokemon': loadPokemon,
@@ -1555,6 +1556,7 @@ function showGymDetails(id) {
     var data = $.ajax({
         url: 'gym_data',
         type: 'GET',
+        timeout: 10000,
         data: {
             'id': id
         },
