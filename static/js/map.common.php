12983d651a5f2376d567988ef34d47c344f9441a
Glenn Carremans <glenn.carremans@appwise.be>
2017-06-16 19:09:14 +0200

    Fixed map.common.js caching

--- static/js/map.common.php
+++ static/js/map.common.php
@@ -1,6 +1,18 @@
 <?php
 include('../../config.php');
 header('Content-type: text/javascript');
+
+$last_modified_time = filemtime("map.common.php");
+$etag = md5_file("map.common.php");
+// always send headers
+header("Last-Modified: ".gmdate("D, d M Y H:i:s", $last_modified_time)." GMT");
+header("Etag: $etag");
+// exit if not modified
+if (@strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) == $last_modified_time ||
+    @trim($_SERVER['HTTP_IF_NONE_MATCH']) == $etag) {
+    header("HTTP/1.1 304 Not Modified");
+    exit;
+}
 ?>
 'use strict';
 
