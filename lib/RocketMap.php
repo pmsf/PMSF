d64f694e2d22b0a4d5e5d446b99d0eb92c5ab94e
Glenn Carremans <glenn.carremans@appwise.be>
2018-01-29 20:35:09 +0100

    Bug fix RM weather overlay

--- lib/RocketMap.php
+++ lib/RocketMap.php
@@ -5,6 +5,7 @@ namespace Scanner;
 class RocketMap extends Scanner
 {
     private $cpMultiplier;
+
     public function __construct()
     {
         parent::__construct();
@@ -641,16 +642,16 @@ class RocketMap extends Scanner
         }
     }
 
-    public function get_weather($updated=null)
+    public function get_weather($updated = null)
     {
         global $db;
-        $query = "SELECT * FROM weather ORDER BY id ASC";
+        $query = "SELECT * FROM weather";
         $weathers = $db->query($query)->fetchAll(\PDO::FETCH_ASSOC);
         foreach ($weathers as $weather) {
-            $data["weather_".$weather['s2_cell_id']] = $weather;
+            $data["weather_" . $weather['s2_cell_id']] = $weather;
             // force re-bind of gameplay_weather to condition
-            $data["weather_".$weather['s2_cell_id']]['condition'] = $data["weather_".$weather['s2_cell_id']]['gameplay_weather'];
-            unset($data["weather_".$weather['s2_cell_id']]['gameplay_weather']);
+            $data["weather_" . $weather['s2_cell_id']]['condition'] = $data["weather_" . $weather['s2_cell_id']]['gameplay_weather'];
+            unset($data["weather_" . $weather['s2_cell_id']]['gameplay_weather']);
         }
         return $data;
     }
@@ -658,41 +659,41 @@ class RocketMap extends Scanner
     private function setCpMultiplier()
     {
         $this->cpMultiplier = array(
-            1 =>0.094,
-            2 =>0.16639787,
-            3 =>0.21573247,
-            4 =>0.25572005,
-            5 =>0.29024988,
-            6 =>0.3210876,
-            7 =>0.34921268,
-            8 =>0.37523559,
-            9 =>0.39956728,
-            10 =>0.42250001,
-            11 =>0.44310755,
-            12 =>0.46279839,
-            13 =>0.48168495,
-            14 =>0.49985844,
-            15 =>0.51739395,
-            16 =>0.53435433,
-            17 =>0.55079269,
-            18 =>0.56675452,
-            19 =>0.58227891,
-            20 =>0.59740001,
-            21 =>0.61215729,
-            22 =>0.62656713,
-            23 =>0.64065295,
-            24 =>0.65443563,
-            25 =>0.667934,
-            26 =>0.68116492,
-            27 =>0.69414365,
-            28 =>0.70688421,
-            29 =>0.71939909,
-            30 =>0.7317,
-            31 =>0.73776948,
-            32 =>0.74378943,
-            33 =>0.74976104,
-            34 =>0.75568551,
-            35 =>0.76156384
+            1 => 0.094,
+            2 => 0.16639787,
+            3 => 0.21573247,
+            4 => 0.25572005,
+            5 => 0.29024988,
+            6 => 0.3210876,
+            7 => 0.34921268,
+            8 => 0.37523559,
+            9 => 0.39956728,
+            10 => 0.42250001,
+            11 => 0.44310755,
+            12 => 0.46279839,
+            13 => 0.48168495,
+            14 => 0.49985844,
+            15 => 0.51739395,
+            16 => 0.53435433,
+            17 => 0.55079269,
+            18 => 0.56675452,
+            19 => 0.58227891,
+            20 => 0.59740001,
+            21 => 0.61215729,
+            22 => 0.62656713,
+            23 => 0.64065295,
+            24 => 0.65443563,
+            25 => 0.667934,
+            26 => 0.68116492,
+            27 => 0.69414365,
+            28 => 0.70688421,
+            29 => 0.71939909,
+            30 => 0.7317,
+            31 => 0.73776948,
+            32 => 0.74378943,
+            33 => 0.74976104,
+            34 => 0.75568551,
+            35 => 0.76156384
         );
     }
 }
