32769e1c65fc0e09267d3aedf37fb979fa37a60d
Glenn Carremans <glenn.carremans@appwise.be>
2018-02-28 15:17:41 +0100

    Bug fix stop sponsored gym being removed from EX filter

--- static/js/map.js
+++ static/js/map.js
@@ -1764,7 +1764,7 @@ function processGyms(i, item) {
         return true
     }
 
-    if (Store.get('exEligible') && item.park === null) {
+    if (Store.get('exEligible') && item.park === null && item.park === 0) {
         removeGymFromMap(item['gym_id'])
         return true
     }
