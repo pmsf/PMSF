546a37b9ac8f39a69cb0e4307f4aaa867860e18a
James Harland <me@jamesharland.com>
2018-03-27 11:08:13 +0100

    Added webhook support

--- manual_raid_submit.php
+++ manual_raid_submit.php
@@ -1,7 +1,7 @@
 <?php
 $timing['start'] = microtime(true);
 include('config/config.php');
-global $map, $fork, $db, $raid_bosses;
+global $map, $fork, $db, $raid_bosses, $webhookUrl, $sendWebhook;
 $pokemonId = !empty($_POST['pokemonId']) ? $_POST['pokemonId'] : 0;
 $gymId = !empty($_POST['gymId']) ? $_POST['gymId'] : 0;
 $mins = !empty($_POST['mins']) ? $_POST['mins'] : 0;
@@ -24,13 +24,11 @@ $d["timestamp"] = $now->getTimestamp();
 
 //$db->debug();
 // fetch fort_id
-// $gym = $db->exec("SELECT * FROM forts where external_id =?", [$_POST['gymId']])->fetchAll();
-$gymId = $db->get("forts", ['id'], ['external_id' => $gymId])['id'];
-//print_r($gym);
+$gym = $db->get("forts", ['id', 'name', 'lat', 'lon'], ['external_id' => $gymId]);
+$gymId = $gym['id'];
 
 $add_seconds = ($mins * 60) + $secs;
 $time_battle = time() + $add_seconds;
-//$time_end = $time_battle + $forty_five;
 $time_end = $time_battle + $forty_five;
 // fake the battle start and spawn times cuz rip hashing :(
 $time_spawn = time() - $forty_five;
@@ -38,6 +36,7 @@ $extId = rand(0, 65535) . rand(0, 65535);
 $level = 0;
 if(strpos($pokemonId,'egg_') !== false){
     $level = (int)substr($pokemonId,4,1);
+    $time_spawn = time() + $add_seconds;
 }
 
 
@@ -64,11 +63,48 @@ if (array_key_exists($pokemonId, $raid_bosses)) {
     $cols['move_2'] = 133;
     $cols['level'] = $raid_bosses[$pokemonId]['level']; // struggle :(
     $cols['cp'] = $raid_bosses[$pokemonId]['cp'];
-} else {
-    // no boss matched
+} elseif($cols['level'] === 0) {
+    // no boss or egg matched
+    http_response_code(500);
 }
 $db->query('DELETE FROM raids WHERE fort_id IN ( SELECT id FROM forts WHERE external_id = ":external")', [':external' => $gymId]);
 $db->insert("raids", $cols);
+
+if ($sendWebhook === true) {
+    // webhook stuff:
+    // build webhook array:
+
+    $webhook = [
+        'message' => [
+            'gym_id' => $cols['external_id'],
+            'pokemon_id' => $cols['pokemon_id'],
+            'cp' => $cols['cp'],
+            'move_1' => $cols['move_1'],
+            'move_2' => $cols['move_2'],
+            'level' => $cols['level'],
+            'latitude' => $gym['lat'],
+            'longitude' => $gym['lon'],
+            'raid_end' => $time_end,
+            'team' => 0,
+            'name' => $gym['name']
+        ],
+        'type' => 'raid'
+    ];
+    if(strpos($pokemonId,'egg_') !== false) {
+        $webhook['message']['raid_begin'] = $time_spawn;
+    }
+
+
+    $c = curl_init($webhookUrl);
+    curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($c, CURLOPT_POST, true);
+    curl_setopt($c, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($c, CURLOPT_HTTPHEADER, ['Content-type: application/json', 'User-Agent: python-requests/2.18.4']);
+    curl_setopt($c, CURLOPT_POSTFIELDS, json_encode($webhook));
+    curl_exec($c);
+    curl_close($c);
+}
+
 $jaysson = json_encode($d);
 echo $jaysson;
-//header("Location: /");
