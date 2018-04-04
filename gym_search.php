d9ffaea42d66b48c85e8cb0a1c0d3401bd12be78
iroken22 <compunerd451@gmail.com>
2018-04-04 13:09:21 -0500

    Added PostGre Support for gym search. I still recommend creating an index instead.

--- gym_search.php
+++ gym_search.php
@@ -2,8 +2,12 @@
 include('config/config.php');
 global $map, $fork, $db;
 $term = !empty($_POST['term']) ? $_POST['term'] : '';
-
-$gyms = $db->select("forts", ['id', 'name', 'lat', 'lon', 'url'], ['name[~]' => $term, 'LIMIT' => 20]);
+if($db->info()['driver'] === 'pgsql') {
+    $gyms = $db->query("SELECT id,name,lat,lon,url FROM forts WHERE LOWER(name) LIKE :name LIMIT 20",  [':name' => "%" . strtolower($term) . "%"])->fetchAll();
+}
+else {
+  $gyms = $db->select("forts", ['id', 'name', 'lat', 'lon', 'url'], ['name[~]' => $term, 'LIMIT' => 20]);
+}
 
 // set content type
 header('Content-Type: application/json');
