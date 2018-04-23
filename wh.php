9fb544e22d861837596abdbfd67b4e064330a6d3
Martin Falk <falken_liten@hotmail.com>
2018-04-23 21:59:13 +0200

    styleci

--- wh.php
+++ wh.php
@@ -11,22 +11,22 @@ $quantity = !empty($json["quantity"]) ? $json["quantity"] : null;
 $timestamp = time();
 
 if (($noNativeLogin === false || $noDiscordLogin === false) && !empty($id) && !empty($product_id) && !empty($email) && !empty($value) && !empty($quantity)) {
-	$signatureFromHeader = getallheaders();
-	$signature = hash_hmac('sha512', file_get_contents('php://input'), $sellyWebhookSecret);
+    $signatureFromHeader = getallheaders();
+    $signature = hash_hmac('sha512', file_get_contents('php://input'), $sellyWebhookSecret);
 
-	if (hash_equals($signature, $signatureFromHeader['X-Selly-Signature'])) {
-		$db->insert("payments", [
-			"selly_id" => $id,
-			"product_id" => $product_id,
-			"email" => $email,
-			"value" => $value,
-			"quantity" => $quantity,
-			"timestamp" => $timestamp
-		]);
+    if (hash_equals($signature, $signatureFromHeader['X-Selly-Signature'])) {
+        $db->insert("payments", [
+            "selly_id" => $id,
+            "product_id" => $product_id,
+            "email" => $email,
+            "value" => $value,
+            "quantity" => $quantity,
+            "timestamp" => $timestamp
+        ]);
 
-		$logMsg = "INSERT INTO payments (selly_id, product_id, email, value, quantity, timestamp) VALUES ('{$id}', '{$product_id}', '{$email}, '{$value}, '{$quantity}, '{$timestamp}');\r\n";
-		file_put_contents($logfile, $logMsg, FILE_APPEND);
-	}
+        $logMsg = "INSERT INTO payments (selly_id, product_id, email, value, quantity, timestamp) VALUES ('{$id}', '{$product_id}', '{$email}, '{$value}, '{$quantity}, '{$timestamp}');\r\n";
+        file_put_contents($logfile, $logMsg, FILE_APPEND);
+    }
 } else {
     header("Location: .");
 }
