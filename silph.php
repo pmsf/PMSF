edc555276edf28744a31a734e01b8ba3384ec7b1
iroken22 <compunerd451@gmail.com>
2018-05-02 13:15:34 -0500

    updated silph query for postgre

--- silph.php
+++ silph.php
@@ -34,7 +34,11 @@ foreach ( $nestCoords as $c ) {
 
     $nests = json_decode( $result, true )['localMarkers'];
     foreach ( $nests as $nest ) {
-        $query = "INSERT INTO nests (nest_id, lat, lon, pokemon_id, updated,type) VALUES (" . $nest['id'] . "," . $nest['lt'] . "," . $nest['ln'] . "," . $nest['pokemon_id'] . "," . time() . ",1) ON DUPLICATE KEY UPDATE pokemon_id=" . $nest['pokemon_id'] . ", updated=" . time() . ", type=1";
+        if($db->info()['driver'] === 'pgsql'){
+            $query = "INSERT INTO nests (nest_id, lat, lon, pokemon_id, updated,type) VALUES (" . $nest['id'] . "," . $nest['lt'] . "," . $nest['ln'] . "," . $nest['pokemon_id'] . "," . time() . ",1) ON CONFLICT (nest_id) DO UPDATE SET pokemon_id=" . $nest['pokemon_id'] . ", updated=" . time() . ", type=1";
+        } else{
+            $query = "INSERT INTO nests (nest_id, lat, lon, pokemon_id, updated,type) VALUES (" . $nest['id'] . "," . $nest['lt'] . "," . $nest['ln'] . "," . $nest['pokemon_id'] . "," . time() . ",1) ON DUPLICATE KEY UPDATE pokemon_id=" . $nest['pokemon_id'] . ", updated=" . time() . ", type=1";
+        }
         $db->query( $query )->fetchAll();
     }
 }
