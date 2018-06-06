e7f820489e1811afb9e56eaebbc0d8b57a3b68ed
whitewillem <jezuswateengedoe@gmail.com>
2018-06-06 22:43:28 +0200

    fix indent

--- static/js/map.js
+++ static/js/map.js
@@ -1332,7 +1332,7 @@ function setupPokestopMarker(item) {
     marker.infoWindow = new google.maps.InfoWindow({
         content: pokestopLabel(item['lure_expiration'], item['latitude'], item['longitude'], item['pokestop_name'], item['lure_user'], item['pokestop_id'], item['quest_id'], item['reward_id']),
         disableAutoPan: true,
-		pixelOffset: new google.maps.Size(0, -20)
+        pixelOffset: new google.maps.Size(0, -20)
     })
 
     addListeners(marker)
