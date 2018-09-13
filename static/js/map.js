82ca3f36f5b3c2aed679202a15f3c8d9adc6c413
whitewillem <jezuswateengedoe@gmail.com>
2018-09-13 23:30:19 +0200

    fix raid timers for manual submit

--- static/js/map.js
+++ static/js/map.js
@@ -2957,52 +2957,7 @@ function generateRaidModal() {
 
 function generateTimerLists() {
     var html = '<select name="egg_time" class="egg_time" style="display:none;">' +
-        '<option value="60" selected>60</option>' +
-        '<option value="59">59</option>' +
-        '<option value="58">58</option>' +
-        '<option value="57">57</option>' +
-        '<option value="56">56</option>' +
-        '<option value="55">55</option>' +
-        '<option value="54">54</option>' +
-        '<option value="53">53</option>' +
-        '<option value="52">52</option>' +
-        '<option value="51">51</option>' +
-        '<option value="50">50</option>' +
-        '<option value="49">49</option>' +
-        '<option value="48">48</option>' +
-        '<option value="47">47</option>' +
-        '<option value="46">46</option>' +
-        '<option value="45">45</option>' +
-        '<option value="44">44</option>' +
-        '<option value="43">43</option>' +
-        '<option value="42">42</option>' +
-        '<option value="41">41</option>' +
-        '<option value="40">40</option>' +
-        '<option value="39">39</option>' +
-        '<option value="38">38</option>' +
-        '<option value="37">37</option>' +
-        '<option value="36">36</option>' +
-        '<option value="35">35</option>' +
-        '<option value="34">34</option>' +
-        '<option value="33">33</option>' +
-        '<option value="32">32</option>' +
-        '<option value="31">31</option>' +
-        '<option value="30">30</option>' +
-        '<option value="29">29</option>' +
-        '<option value="28">28</option>' +
-        '<option value="27">27</option>' +
-        '<option value="26">26</option>' +
-        '<option value="25">25</option>' +
-        '<option value="24">24</option>' +
-        '<option value="23">23</option>' +
-        '<option value="22">22</option>' +
-        '<option value="21">21</option>' +
-        '<option value="20">20</option>' +
-        '<option value="19">19</option>' +
-        '<option value="18">18</option>' +
-        '<option value="17">17</option>' +
-        '<option value="16">16</option>' +
-        '<option value="15">15</option>' +
+        '<option value="15" selected>15</option>' +
         '<option value="14">14</option>' +
         '<option value="13">13</option>' +
         '<option value="12">12</option>' +
@@ -3019,7 +2974,22 @@ function generateTimerLists() {
         '<option value="1">1</option>' +
         '</select>' +
         '<select name="mon_time" class="mon_time" style="display:none;">' +
-        '<option value="45" selected>45</option>' +
+        '<option value="60" selected>60</option>' +
+        '<option value="59">59</option>' +
+        '<option value="58">58</option>' +
+        '<option value="57">57</option>' +
+        '<option value="56">56</option>' +
+        '<option value="55">55</option>' +
+        '<option value="54">54</option>' +
+        '<option value="53">53</option>' +
+        '<option value="52">52</option>' +
+        '<option value="51">51</option>' +
+        '<option value="50">50</option>' +
+        '<option value="49">49</option>' +
+        '<option value="48">48</option>' +
+        '<option value="47">47</option>' +
+        '<option value="46">46</option>' +
+        '<option value="45">45</option>' +
         '<option value="44">44</option>' +
         '<option value="43">43</option>' +
         '<option value="42">42</option>' +
