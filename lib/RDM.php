9a0bdbec2c418e8d4eb6eaec5d96306a63f4ec7c
jepke <sjef@vanzee.land>
2018-10-06 00:42:17 +0200

    forgotten

--- lib/RDM.php
+++ lib/RDM.php
@@ -72,7 +72,7 @@ class RDM extends Scanner
         }
         $encSql = '';
         if ($encId != 0) {
-            $encSql = " OR (spawn_id = " . $encId . " AND lat > '" . $swLat . "' AND lon > '" . $swLng . "' AND lat < '" . $neLat . "' AND lon < '" . $neLng . "' AND expire_timestamp > '" . $params[':time'] . "')";
+            $encSql = " OR (id = " . $encId . " AND lat > '" . $swLat . "' AND lon > '" . $swLng . "' AND lat < '" . $neLat . "' AND lon < '" . $neLng . "' AND expire_timestamp > '" . $params[':time'] . "')";
         }
         return $this->query_active($select, $conds, $params, $encSql);
     }
