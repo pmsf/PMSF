e80dc9d125f668c6d0bb7bf56c6d2fd77c860b14
Jepke <sjef@vanzee.land>
2018-10-15 12:14:22 +0200

    Customizable sprite repo

--- static/js/map.common.js
+++ static/js/map.common.js
@@ -1232,11 +1232,11 @@ function getPokemonSprite(index, sprite, displayHeight, weather = 0, encounterFo
     var html = ''
     if (copyrightSafe === false) {
         if (weather === 0) {
-            html = '<img src="https://raw.githubusercontent.com/whitewillem/PogoAssets/resized/icons_large/pokemon_icon_' + pokemonIdStr + '_' + formStr + '.png" style="width:' + scaledIconSizeWidth + 'px;height:auto;"/>'
+            html = '<img src="' + iconRepository + 'pokemon_icon_' + pokemonIdStr + '_' + formStr + '.png" style="width:' + scaledIconSizeWidth + 'px;height:auto;"/>'
         } else if (boostedMons[weather].indexOf(pokemonId) === -1) {
-            html = '<img src="https://raw.githubusercontent.com/whitewillem/PogoAssets/resized/icons_large/pokemon_icon_' + pokemonIdStr + '_' + formStr + '.png" style="width:' + scaledIconSizeWidth + 'px;height:auto;"/>'
+            html = '<img src="' + iconRepository + 'pokemon_icon_' + pokemonIdStr + '_' + formStr + '.png" style="width:' + scaledIconSizeWidth + 'px;height:auto;"/>'
         } else {
-            html = '<img src="https://raw.githubusercontent.com/whitewillem/PogoAssets/resized/icons_large/pokemon_icon_' + pokemonIdStr + '_' + formStr + '.png" style="width:' + scaledIconSizeWidth + 'px;height:auto;"/>' +
+            html = '<img src="' + iconRepository + 'pokemon_icon_' + pokemonIdStr + '_' + formStr + '.png" style="width:' + scaledIconSizeWidth + 'px;height:auto;"/>' +
             '<img src="static/weather/i-' + weather + '.png" style="width:' + scaledWeatherIconSizeWidth + 'px;height:auto;position:absolute;top:-' + scaledWeatherIconSizeWidth + 'px;right:0px;"/>'
         }
     } else {
