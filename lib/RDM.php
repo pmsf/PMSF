42788414989b6996792442f2e249572e1483163b
Jepke <sjef@vanzee.land>
2018-10-31 10:12:51 +0100

    Fix forms when 0 in RDM

--- lib/RDM.php
+++ lib/RDM.php
@@ -182,8 +182,7 @@ class RDM extends Scanner
             $pokemon["pokemon_id"] = intval($pokemon["pokemon_id"]);
             $pokemon["pokemon_name"] = i8ln($this->data[$pokemon["pokemon_id"]]['name']);
             $pokemon["pokemon_rarity"] = i8ln($this->data[$pokemon["pokemon_id"]]['rarity']);
-
-            if (isset($pokemon["form"])) {
+            if (isset($pokemon["form"]) && $pokemon["form"] > 0) {
                 $forms = $this->data[$pokemon["pokemon_id"]]["forms"];
                 foreach ($forms as $f => $v) {
                     if ($pokemon["form"] === $v['protoform']) {
