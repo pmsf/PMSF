0db4ea6048bbb539dc96fd79729ad2e52f3054b8
Wheel <jepke@users.noreply.github.com>
2018-11-26 20:04:11 +0100

    Manual DB fixes

--- cron/nest_cron.php
+++ cron/nest_cron.php
@@ -10,14 +10,14 @@ if($noManualNests === true){
 $nestMigrationInterval = 14;
 $days = floor((time() - $migrationDay)/86400) % $nestMigrationInterval;
 
-    $db->update('nests',['pokemon_id' => 0, 'updated' => time(),'type' => 0], ['pokemon_id[!]' => 0]);
+    $manualdb->update('nests',['pokemon_id' => 0, 'updated' => time(),'type' => 0], ['pokemon_id[!]' => 0]);
 if ($days === 0){
     $timeNow = date('U');
     $deleteBefore = $timeNow - ($deleteNestsOlderThan * 24 * 60 * 60);
-    $db->delete('nests',['updated[<]' => $deleteBefore]);
+    $manualdb->delete('nests',['updated[<]' => $deleteBefore]);
     echo 'outdatet nest deleted';
 
-    $db->update('nests',['pokemon_id' => 0, 'updated' => time(),'type' => 0], ['pokemon_id[!]' => 0]);
+    $manualdb->update('nests',['pokemon_id' => 0, 'updated' => time(),'type' => 0], ['pokemon_id[!]' => 0]);
     echo 'updated new nests';
 } else {
     echo 'No nests migration today. ' . ((($days = 14 - $days)>13)?$days-14:$days) .  'days to go.';
