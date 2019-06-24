a378c39c5afff8882bdb9dae5ae0b25c0419dad4
whitewillem <jezuswateengedoe@gmail.com>
2019-06-24 13:56:09 +0200

    - add 'updated', 'submitted by' and inn 'type' to database - add images

--- sql/manualdb/update06_manualdb.sql
+++ sql/manualdb/update06_manualdb.sql
@@ -4,6 +4,9 @@ CREATE TABLE IF NOT EXISTS `inn` (
   `lon` double(18,14) DEFAULT NULL,
   `name` varchar(128) COLLATE utf8mb4_bin DEFAULT NULL,
   `url` varchar(200) COLLATE utf8mb4_bin DEFAULT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 0,
+  `updated` bigint(20) DEFAULT NULL,
+  `submitted_by` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
@@ -13,6 +16,8 @@ CREATE TABLE IF NOT EXISTS `fortress` (
   `lon` double(18,14) DEFAULT NULL,
   `name` varchar(128) COLLATE utf8mb4_bin DEFAULT NULL,
   `url` varchar(200) COLLATE utf8mb4_bin DEFAULT NULL,
+  `updated` bigint(20) DEFAULT NULL,
+  `submitted_by` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
@@ -22,5 +27,7 @@ CREATE TABLE IF NOT EXISTS `greenhouse` (
   `lon` double(18,14) DEFAULT NULL,
   `name` varchar(128) COLLATE utf8mb4_bin DEFAULT NULL,
   `url` varchar(200) COLLATE utf8mb4_bin DEFAULT NULL,
+  `updated` bigint(20) DEFAULT NULL,
+  `submitted_by` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
