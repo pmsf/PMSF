84dcf8b7f04011c436773a37ee2f450e7c484e14
whitewillem <jezuswateengedoe@gmail.com>
2019-07-12 15:39:32 +0200

    fix monocle lib, use inn type images (no submit yet)

--- lib/Monocle_MAD.php
+++ lib/Monocle_MAD.php
@@ -140,6 +140,82 @@ class Monocle_MAD extends Monocle
         return $this->query_active($select, $conds, $params);
     }
 
+    public function query_active($select, $conds, $params, $encSql = '')
+    {
+        global $db;
+
+        $query = "SELECT :select
+        FROM sightings
+        WHERE :conditions ORDER BY lat,lon";
+
+        $query = str_replace(":select", $select, $query);
+        $query = str_replace(":conditions", '(' . join(" AND ", $conds) . ')' . $encSql, $query);
+        $pokemons = $db->query($query, $params)->fetchAll(\PDO::FETCH_ASSOC);
+        $data = array();
+        $i = 0;
+        $lastlat = 0;
+        $lastlon=0;
+        $lasti = 0;
+
+        foreach ($pokemons as $pokemon) {
+            $pokemon["latitude"] = floatval($pokemon["latitude"]);
+            $pokemon["longitude"] = floatval($pokemon["longitude"]);
+            $lastlat = floatval($pokemon["latitude"]);
+            $lastlon = floatval($pokemon["longitude"]);
+            if (abs($pokemon["latitude"] - $lastlat) < 0.0001 && abs($pokemon["longitude"] - $lastlon) < 0.0001){
+                $lasti = $lasti + 1;
+            } else {
+                $lasti = 0;
+            }
+            $pokemon["latitude"] = $pokemon["latitude"] + 0.0003*cos(deg2rad($lasti*45));
+            $pokemon["longitude"] = $pokemon["longitude"] + 0.0003*sin(deg2rad($lasti*45));
+            $pokemon["disappear_time"] = $pokemon["disappear_time"] * 1000;
+
+            $pokemon["weight"] = isset($pokemon["weight"]) ? floatval($pokemon["weight"]) : null;
+            $pokemon["height"] = isset($pokemon["height"]) ? floatval($pokemon["height"]) : null;
+
+            $pokemon["individual_attack"] = isset($pokemon["individual_attack"]) ? intval($pokemon["individual_attack"]) : null;
+            $pokemon["individual_defense"] = isset($pokemon["individual_defense"]) ? intval($pokemon["individual_defense"]) : null;
+            $pokemon["individual_stamina"] = isset($pokemon["individual_stamina"]) ? intval($pokemon["individual_stamina"]) : null;
+
+            $pokemon["weather_boosted_condition"] = isset($pokemon["weather_boosted_condition"]) ? intval($pokemon["weather_boosted_condition"]) : 0;
+            $pokemon['expire_timestamp_verified'] = null;
+            $pokemon["pokemon_id"] = intval($pokemon["pokemon_id"]);
+            $pokemon["pokemon_name"] = i8ln($this->data[$pokemon["pokemon_id"]]['name']);
+            $pokemon["pokemon_rarity"] = i8ln($this->data[$pokemon["pokemon_id"]]['rarity']);
+
+            if (isset($pokemon["form"]) && $pokemon["form"] > 0) {
+                $forms = $this->data[$pokemon["pokemon_id"]]["forms"];
+                foreach ($forms as $f => $v) {
+                    if ($pokemon["form"] === $v['protoform']) {
+                        $types = $v['formtypes'];
+                        foreach ($v['formtypes'] as $ft => $v) {
+                            $types[$ft]['type'] = i8ln($v['type']);
+                        }
+                        $pokemon["pokemon_types"] = $types;
+                    } else if ($pokemon["form"] === $v['assetsform']) {
+                        $types = $v['formtypes'];
+                        foreach ($v['formtypes'] as $ft => $v) {
+                            $types[$ft]['type'] = i8ln($v['type']);
+                        }
+                        $pokemon["pokemon_types"] = $types;
+                    }
+                }
+            } else {
+                $types = $this->data[$pokemon["pokemon_id"]]["types"];
+                foreach ($types as $k => $v) {
+                    $types[$k]['type'] = i8ln($v['type']);
+                }
+                $pokemon["pokemon_types"] = $types;
+            }
+            $data[] = $pokemon;
+
+            unset($pokemons[$i]);
+            $i++;
+        }
+        return $data;
+    }
+
     public function get_stops($qpeids, $qieids, $swLat, $swLng, $neLat, $neLng, $tstamp = 0, $oSwLat = 0, $oSwLng = 0, $oNeLat = 0, $oNeLng = 0, $lures, $quests, $dustamount)
     {
         $conds = array();
