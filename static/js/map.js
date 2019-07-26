9b6ec5e62775f697c64cc328bcfd132dca7af539
whitewillem <jezuswateengedoe@gmail.com>
2019-07-26 13:56:39 +0200

    Minor text fixes

--- static/js/map.js
+++ static/js/map.js
@@ -1430,10 +1430,10 @@ function pokestopLabel(item) {
         str += '<center><div><i class="fas fa-binoculars submit-quest" onclick="openQuestModal(event);" data-id="' + item['pokestop_id'] + '">' + i8ln('Add Quest') + '</i></div></center>'
     }
     if (!noRenamePokestops) {
-        str += '<center><div><i class="fas fa-edit rename-pokestop" style="margin-top: 2px; vertical-align: middle; font-size: 1.5em;" onclick="openRenamePokestopModal(event);" data-id="' + item['pokestop_id'] + '">' + i8ln('Rename Pokestop') + '</i></div></center>'
+        str += '<center><div><i class="fas fa-edit rename-pokestop" onclick="openRenamePokestopModal(event);" data-id="' + item['pokestop_id'] + '">' + i8ln('Rename Pokestop') + '</i></div></center>'
     }
     if (!noConvertPokestops) {
-        str += '<center><div><i class="fas fa-sync-alt convert-pokestop" style="margin-top: 2px; vertical-align: middle; font-size: 1.5em;" onclick="openConvertPokestopModal(event);" data-id="' + item['pokestop_id'] + '">' + i8ln('Convert to Gym') + '</i></div></center>'
+        str += '<center><div><i class="fas fa-sync-alt convert-pokestop" onclick="openConvertPokestopModal(event);" data-id="' + item['pokestop_id'] + '">' + i8ln('Convert to Gym') + '</i></div></center>'
     }
     str += '<div><center>' +
         '<a href="javascript:void(0)" onclick="javascript:openMapDirections(' + item['latitude'] + ',' + item['longitude'] + ')" title="' + i8ln('View in Maps') + '"><i class="fas fa-road"></i> ' + item['latitude'] + ', ' + item['longitude'] + '</a> - <a href="./?lat=' + item['latitude'] + '&lon=' + item['longitude'] + '&zoom=16"><i class="far fa-share-square" aria-hidden="true" style="position:relative;top:3px;left:0px;color:#26c300;font-size:20px;"></i></a>' +
