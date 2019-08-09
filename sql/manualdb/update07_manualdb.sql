b04fd13cd1e3d2029ea96ae2cf95d408286e6b03
Jepke <sjef@vanzee.land>
2019-08-09 04:41:45 +0200

    Revert "SQL update07"
    This reverts commit a0b09f0ad36d5b4791c5dc8cbeca8cbf1ca02b32.

--- sql/manualdb/update07_manualdb.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `poi`  ADD `poiimageurl` VARCHAR(200) NULL  AFTER `notes`,  ADD `poiimagedeleteurl` VARCHAR(200) NULL  AFTER `poiimageurl`,  ADD `poisurroundingurl` VARCHAR(200) NULL  AFTER `poiimagedeleteurl`,  ADD `poisurroundingdeleteurl` VARCHAR(200) NULL  AFTER `poisurroundingurl`;
-
