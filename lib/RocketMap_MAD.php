e1392dcdbbf383a01222f5c608c21ed998c6dcca
whitewillem <jezuswateengedoe@gmail.com>
2019-09-13 13:45:04 +0200

    ;

--- lib/RocketMap_MAD.php
+++ lib/RocketMap_MAD.php
@@ -199,7 +199,7 @@ class RocketMap_MAD extends RocketMap
             global $noDittoDetection, $possibleDitto;
             if (!$noDittoDetection && isset($pokemon["weather_boosted_condition"]) && isset($pokemon["cp_multiplier"])) {
                 if (in_array($pokemon["pokemon_id"], $possibleDitto) && $pokemon["weather_boosted_condition"] > 0 && $pokemon["cp_multiplier"] !== null) {
-                    $multipliedMultiplier = $pokemon["cp_multiplier"] * 1000
+                    $multipliedMultiplier = $pokemon["cp_multiplier"] * 1000;
                     if (($multipliedMultiplier <= 256) || ($pokemon["individual_attack"] < 4 || $pokemon["individual_defense"] < 4 || $pokemon["individual_stamina"] < 4)) {
                         $pokemon["pokemon_id"] = 132;
                         $pokemon["form"] = 0;
