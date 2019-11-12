4909c9330bcf399c4a9e1cedaae83c247dfc513f
whitewillem <jezuswateengedoe@gmail.com>
2019-11-12 17:26:30 +0100

    add $hideNestCoords

--- config/default.php
+++ config/default.php
@@ -416,6 +416,7 @@ $noEditCommunity = true;
 //-----------------------------------------------------
 $noNests = true;                                                   // true/false
 $enableNests = 'false';                                             // true/false
+$hideNestCoords = false;
 $noManualNests = true;
 $noDeleteNests = true;
 $deleteNestsOlderThan = 42;					    // days after not updated nests are removed from database by nest cron
