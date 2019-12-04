6a73b90f91a322795bff08737a32d035694530e5
whitewillem <jezuswateengedoe@gmail.com>
2019-12-04 15:27:20 +0100

    Add access level to session, remove query from access config, use session instead of query result.

--- config/example.access-config.php
+++ config/example.access-config.php
@@ -7,9 +7,6 @@
 // - TIP: Patreon has a Discord integration to set discord roles (read: user levels) when someone makes a pledge. This works perfect in combination with authbot and this example access config.
 // - EVERYTHING in this file overwrites config.php.
 
-/* Query the access level of the logged user (DO NOT EDIT OR REMOVE THIS) */
-$userAccessLevel = $manualdb->get("users", [ 'access_level' ], [ 'user' => $_SESSION['user']->user ]);
-
 /* Define access levels. (must match authbot config)*/
 $userLevel = 0;
 $raidLevel = 1;
@@ -17,27 +14,27 @@ $questLevel = 2;
 $ivLevel = 3;
 $ownerLevel = 4;
 
-/* If query result matches defined user level, enable/disable something. */
-if ($userAccessLevel['access_level'] == $userLevel) {
+/* If $_SESSION['user']->access_level matches defined user level, enable/disable something. */
+if ($_SESSION['user']->access_level == $userLevel) {
     $noMotd = false;
     $motdTitle = "Welcome " . $_SESSION['user']->user . "!";
     $patreonUrl = 'https://yourPatreonURL.com';
     $motdContent = "<center>You can get access to Raids, Quests, Pok&eacute;mon and IV if you make a pledge to our Patreon.<br>
                     <a class='button' style='outline:none;' href='" . $patreonUrl . "'><i class='fab fa-patreon'></i> Pledge</a>
                     </center>";
-} elseif ($userAccessLevel['access_level'] == $raidLevel) {
+} elseif ($_SESSION['user']->access_level == $raidLevel) {
     $noRaids = false;
-} elseif ($userAccessLevel['access_level'] == $questLevel) {
+} elseif ($_SESSION['user']->access_level == $questLevel) {
     $noRaids = false;
     $noQuests = false;
     $noLures = false;
-} elseif ($userAccessLevel['access_level'] == $ivLevel) {
+} elseif ($_SESSION['user']->access_level == $ivLevel) {
     $noRaids = false;
     $noQuests = false;
     $noLures = false;
     $noPokemon = false;
     $noHighLevelData = false;
-} elseif ($userAccessLevel['access_level'] == $ownerLevel) {
+} elseif ($_SESSION['user']->access_level == $ownerLevel) {
     $noRaids = false;
     $noQuests = false;
     $noLures = false;
