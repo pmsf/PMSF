77cc0d3a5f22566476b8c220760421073f0243e9
whitewillem <jezuswateengedoe@gmail.com>
2020-01-04 19:28:42 +0100

    remove trailing space

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,7 +47,7 @@ module.exports = function (grunt) {
                     'static/dist/js/serviceWorker.min.js': 'static/js/serviceWorker.js'
                 }
             }
-        },	
+        },
         javascript_obfuscator: {
             options: {
                 compact: true,
