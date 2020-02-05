7c3e4a6d82511ab68dba05d365002b769e9d2426
whitewillem <jezuswateengedoe@gmail.com>
2020-02-05 21:58:29 +0100

    travis

--- static/js/map.js
+++ static/js/map.js
@@ -1037,7 +1037,7 @@ function createHearts() {
             heartImage: 'https://pngimage.net/wp-content/uploads/2018/06/heart-png-images.png',
             maxHearts: 30,
             minScale: 0.3,
-            draw: function() {
+            draw: function () {
                 this.setCanvasSize()
                 this.ctx.clearRect(0, 0, this.w, this.h)
                 for (var i = 0; i < this.hearts.length; i++) {
@@ -1050,30 +1050,30 @@ function createHearts() {
                 }
                 this.move()
             },
-            move: function() {
-                for(var b = 0; b < this.hearts.length; b++) {
+            move: function () {
+                for (var b = 0; b < this.hearts.length; b++) {
                     var heart = this.hearts[b]
                     heart.y += heart.ys
-                    if(heart.y > this.h) {
+                    if (heart.y > this.h) {
                         heart.x = Math.random() * this.w
                         heart.y = -1 * this.heartHeight
                     }
                 }
             },
-            setCanvasSize: function() {
+            setCanvasSize: function () {
                 this.canvas.width = window.innerWidth
                 this.canvas.height = window.innerHeight
                 this.w = this.canvas.width
                 this.h = this.canvas.height
             },
-            initialize: function() {
+            initialize: function () {
                 this.canvas = $('#valentine-canvas')[0]
                 if (!this.canvas.getContext) {
                     return
                 }
                 this.setCanvasSize()
                 this.ctx = this.canvas.getContext('2d')
-                for(var a = 0; a < this.maxHearts; a++) {
+                for (var a = 0; a < this.maxHearts; a++) {
                     var scale = (Math.random() * (1 - this.minScale)) + this.minScale
                     this.hearts.push({
                         x: Math.random() * this.w,
