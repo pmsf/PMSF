5958b888c6574f939560aaba3f7a827ee5a02276
whitewillem <35378709+whitewillem@users.noreply.github.com>
2020-08-28 00:47:23 +0200

    Update pre-index.php

--- pre-index.php
+++ pre-index.php
@@ -1313,7 +1313,7 @@ if (!$noLoadingScreen) {
                             <option value="3">3</option>
                             <option value="4">4</option>
                             <option value="5">5</option>
-                            <option value="5">5</option>
+                            <option value="6">6</option>
                         </select>
                     </div>';
             }
