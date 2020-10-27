26f201cd66b55f30310eea54e25d71cb78dca3e3
Malte <42342921+ccev@users.noreply.github.com>
2020-10-27 15:42:35 +0100

    improvements to nest table

--- sql/manualdb/update13_manualdb.sql
+++ sql/manualdb/update13_manualdb.sql
@@ -5,6 +5,4 @@ ADD `pokemon_ratio` double DEFAULT 0,
 ADD `polygon_type` tinyint(1) DEFAULT 0,
 ADD `polygon_path` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
 
-ADD `unknown_name` tinyint(1) DEFAULT 0,
-
 DROP `nest_submitted_by`;
