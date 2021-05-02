9c3aa5eed0cc05ee3230e6497fac3aae75f2b6be
whitewillem <jezuswateengedoe@gmail.com>
2021-05-02 13:50:56 +0200

    fix size issues

--- static/js/map.js
+++ static/js/map.js
@@ -5226,7 +5226,7 @@ function updateMap() {
                 var currentCell = $('#currentWeather').data('current-cell')
                 if ((currentWeather) && (currentCell !== currentWeather.s2_cell_id)) {
                     $('#currentWeather').data('current-cell', currentWeather.s2_cell_id)
-                    $('#currentWeather').html('<img src="static/weather/' + currentWeather.condition + '.png" alt="" height="55px"">')
+                    $('#currentWeather').html('<img src="static/weather/' + currentWeather.condition + '.png">')
                 } else if (!currentWeather) {
                     $('#currentWeather').data('current-cell', '')
                     $('#currentWeather').html('')
